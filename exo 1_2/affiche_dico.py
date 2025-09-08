a2d660d6e2cf649f35dd322aeb7025d15c8f35aa
e2400854 <quentin.hartmann@uha.fr>
2025-09-08 15:03:38 +0200

    08/09/2025

--- exo 1_2/affiche_dico.py
+++ exo 1_2/affiche_dico.py
@@ -1,7 +1,7 @@
-def affiche_dico(d: dict, prefixe: str) -> None:
-    for k, v in d.items():
-        print(f"{prefixe} {k}: {v}")
+def affiche_dico(a: dict, b: str) -> None:
+    for k, v in a.items():
+        print(f"{b} -> {k}: {v}")
 
 mon_dictionaire = {"nom" : "albert", "prenom" : "arnauld", "travail" : "professeur"}
-prefixe_texte = "Info ->"
+prefixe_texte = "Info"
 print(affiche_dico(mon_dictionaire, prefixe_texte))
